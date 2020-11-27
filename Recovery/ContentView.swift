4c8a9562dc3084364245422af88f430a152ba41c
abdelmajid rajad <abdelmajid.rajad@gmail.com>
2020-11-27 18:48:47 +0100

    Initial Commit

---
+++ Recovery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recovery
+//
+//  Created by Majid on 27/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
